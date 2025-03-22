acb474b8e0f7fc7e2518ae6bc5be4cfdff7624ab
Avichai Shalom <avichaishalom35@gmail.com>
2025-03-22 12:06:02 +0200

    all working, without constants

--- prog.c
+++ prog.c
@@ -3,19 +3,20 @@
 
 
 int main() {
-	/*
+	
 	encrypt_file_ECB("..\\..\\..\\in.txt", "..\\..\\..\\enc.txt", 0);
 	printf("encrypted ECB\n");
 	decrypt_file_ECB("..\\..\\..\\enc.txt", "..\\..\\..\\dec.txt", 0);
 	printf("decrypted ECB\n");
-	*/
+
 	printf("\n");
-	/*
+	 
 	encrypt_file_CBC("..\\..\\..\\in.txt", "..\\..\\..\\enc.txt", 0);
 	printf("encrypted CBC\n");
 	decrypt_file_CBC("..\\..\\..\\enc.txt", "..\\..\\..\\dec.txt", 0);
 	printf("decrypted CBC\n");
-	*/
+
+	printf("\n");
 
 	encrypt_file_CFB("..\\..\\..\\in.txt", "..\\..\\..\\enc.txt", 0);
 	printf("encrypted CFB\n");
@@ -28,12 +29,13 @@ int main() {
 	printf("encrypted OFB\n");
 	decrypt_file_OFB("..\\..\\..\\enc.txt", "..\\..\\..\\dec.txt", 0);
 	printf("decrypted OFB\n");
-
+	
 	printf("\n");
 
 	encrypt_file_CTR("..\\..\\..\\in.txt", "..\\..\\..\\enc.txt", 0);
 	printf("encrypted CTR\n");
 	decrypt_file_CTR("..\\..\\..\\enc.txt", "..\\..\\..\\dec.txt", 0);
 	printf("decrypted CTR\n");
+
 	return 0;
 }

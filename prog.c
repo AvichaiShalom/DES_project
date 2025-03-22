e385095f5b8e44dc7ef0b1ce8a26d75b90fed3e6
Avichai Shalom <avichaishalom35@gmail.com>
2025-03-22 12:46:33 +0200

    add constants to DES_block

--- prog.c
+++ prog.c
@@ -1,8 +1,30 @@
 #include "DES_modes.h"
+#include "DES_block.h"
 
 
 
 int main() {
+	
+	uint64_t plaintext = 0x123456789ABCDEF0; // קלט לדוגמה
+    uint64_t key = 0x133457799BBCDFF1; // מפתח לדוגמה
+    uint64_t ciphertext, decrypted_plaintext;
+
+    // הצפנה
+    DES_encrypt(plaintext, &ciphertext, key);
+    printf("Ciphertext: 0x%lX\n", ciphertext);
+
+    // פענוח
+    DES_decrypt(ciphertext, &decrypted_plaintext, key);
+    printf("Decrypted plaintext: 0x%lX\n", decrypted_plaintext);
+
+    // בדיקה אם הפענוח תואם לקלט המקורי
+    if (plaintext == decrypted_plaintext) {
+        printf("DES test passed!\n");
+    } else {
+        printf("DES test failed!\n");
+    }
+
+    return 0;
 	
 	encrypt_file_ECB("..\\..\\..\\in.txt", "..\\..\\..\\enc.txt", 0);
 	printf("encrypted ECB\n");
@@ -29,7 +51,7 @@ int main() {
 	printf("encrypted OFB\n");
 	decrypt_file_OFB("..\\..\\..\\enc.txt", "..\\..\\..\\dec.txt", 0);
 	printf("decrypted OFB\n");
-	
+
 	printf("\n");
 
 	encrypt_file_CTR("..\\..\\..\\in.txt", "..\\..\\..\\enc.txt", 0);
